28044eb25f9d44373223a82aaa05b46ed1c85aa7
Moritz Mani <moritz.mani.mm@gmail.com>
2022-09-07 20:19:51 +0200

    initial commit

---
+++ project/app/src/main/java/me/mehdi/phonebook/MapActivity.kt
@@ -0,0 +1,54 @@
+package me.mehdi.phonebook
+
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AutoCompleteTextView
+import me.mehdi.phonebook.databinding.ActivityMapBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import android.widget.ArrayAdapter
+
+class MapActivity : AppCompatActivity() {
+
+    lateinit var mMap : MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mMap = binding.mapView
+        mMap.setTileSource(TileSourceFactory.MAPNIK)
+
+
+
+        Configuration.getInstance().load(applicationContext, getSharedPreferences("phonebook_app", MODE_PRIVATE))
+
+        val longitude = intent.getDoubleExtra("longitude", 8.5476)
+        val latitude = intent.getDoubleExtra("latitude", 47.3764)
+
+
+        val controller = mMap.controller
+
+        val mapPoint = GeoPoint(latitude, longitude)
+
+        controller.setZoom(19)
+
+        controller.animateTo(mapPoint)
+
+        val textView1 = findViewById(R.id.start) as AutoCompleteTextView
+        val suggestions1: Array<out String> = resources.getStringArray(R.array.suggestions)
+        ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, suggestions1).also {
+            adapter -> textView1.setAdapter(adapter)
+        }
+        val textView2 = findViewById(R.id.end) as AutoCompleteTextView
+        val suggestions2: Array<out String> = resources.getStringArray(R.array.suggestions)
+        ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, suggestions2).also {
+            adapter -> textView2.setAdapter(adapter)
+        }
+    }
+}
